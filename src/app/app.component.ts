52e831d9ec51d84dc4ec7d843a22054b54710a9c
Nodir <114328849+Nodir-any@users.noreply.github.com>
2022-10-16 20:14:18 +0500

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import "https://kit.fontawesome.com/59ea9a7e7e.js";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'first-app';
+}
